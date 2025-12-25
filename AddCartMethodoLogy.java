1ee3dbde702375ee9646943dfba0ed8208ec8aef
Abhishek <infobasu96@gmail.com>
2025-12-25 10:37:31 +0530

    ADd

--- AddCartMethodoLogy.java
+++ AddCartMethodoLogy.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.JavascriptExecutor;
 
 public class AddCartMethodoLogy {
 
